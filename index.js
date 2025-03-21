ac745d852d66113f7e98ba4c39ba1ba9c506b54f
deenu-cse <vdeendayal866@gmail.com>
2025-03-21 21:08:19 +0530

    crrrsss

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ const { dbConnect } = require('./utils/db');
 
 app.use(express.json());
 
-app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}));
+app.use(cors());
 
 
 app.use('/', Routes)
